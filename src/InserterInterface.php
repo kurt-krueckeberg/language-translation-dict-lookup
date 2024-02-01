53881b9abf0a6a79d1a270de53aea28f42ef6462
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-01-31 19:46:30 -0500

    $1

--- src/InserterInterface.php
+++ src/InserterInterface.php
@@ -7,5 +7,5 @@ interface InserterInterface {
    function insert_word(WordResultInterface $wrface) : int;
    function insert_verb(WordResultInterface $wrface) : int;
    function insert_noun(WordResultInterface $wrface) : int;
-   function insert_samples(string $word, array $sample_sents) : bool;
+   function insert_examples(string $word, SentencesIterator $iter) : bool;
 }
