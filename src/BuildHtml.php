de029c7d1111e6490e2127ed6a8d8989f3374dc1
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-02-05 18:09:38 -0500

    $1

---
+++ src/BuildHtml.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+
+namespace Vocab;
+
+use \SplFileObject as File; 
+
+class BuildHtml {
+
+     private File  $out;
+     private bool $b_saved;       
+     private string $src;       
+     private string $dest;      
+     private HtmlBuilder $html_builder;      
+
+static private string $out_start = <<<html_eos
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de">
+   <head>
+      <title>German Vocab</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" type="text/css" media="screen" href="css/vocab.css"> 
+      <link rel="stylesheet" type="text/css" media="print" href="css/print-vocab.css"> 
+   </head>
+<body>
+html_eos;
+
+static private string $out_end = <<<html_end
+    </body>
+</html>
+html_end;
+
+    public function __construct(string $ofname, string $src, string $dest)
+    { 
+       $this->b_saved = false;
+       
+       $this->src = $src;
+       
+       $this->dest = $dest;
+
+       $this->out = new File($ofname . ".html", "w"); 
+
+       $this->html_builder = new HtmlBuilder();
+
+       $this->out->fwrite(self::$out_start);
+   }
+ 
+   public function add_definitions(WordResultInterface $wrface) : void
+   {
+      $section = $this->html_builder->build_definitions_section($wrface);
+
+      $this->out->fwrite($section);
+ 
+      return;
+   }
+
+  // to do: make sure the array has both 1.) sample and 2.) its translation.
+   public function add_samples(string $word, SentencesIterator $iter, TranslateInterface $trans) : void 
+   {
+      $section = $this->html_builder->add_samples_section($word, $iter, $trans);
+
+      $this->out->fwrite($section);
+
+      return;
+  } 
+
+  public function __destruct()
+  {
+     $this->save();        
+  } 
+
+  public function save()
+  {
+     if (!$this->b_saved) {
+
+          $this->out->fwrite(self::$out_end);
+          $this->b_saved = true;
+      } 
+  }
+}
