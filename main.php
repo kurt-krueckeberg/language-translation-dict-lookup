d96a8bc80cc65b6d74f11170b2a4d3d54ec36add
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-02-07 14:19:17 -0500

    $1

--- main.php
+++ main.php
@@ -3,68 +3,19 @@
 declare(strict_types=1);
 use \SplFileObject as File;
 
-use Vocab\{Database, SystranTranslator, LeipzigSentenceFetcher, DbTablesMediator, FileReader, Config, ProviderID, Word};
+use Vocab\{FileReader, Config};
 
 include 'vendor/autoload.php';
-
-try {
-    
-    $c = new Config();
-            
-    if (!file_exists($c->lookup_file())) {
-        
-        die($c->lookup_file() . " not found.\n");
-    }
-
-    $file = new FileReader($c->lookup_file());
-    
-    $sys = new SystranTranslator($c);
    
-    $db = new Database($c); 
+ $c = new Config();
+         
+ if (!file_exists($c->lookup_file())) {
+     
+     die($c->lookup_file() . " not found.\n");
+ }
 
-    $sentFetcher = new LeipzigSentenceFetcher($c);
-    
-    foreach ($file as $word) {
-       
-       $iter = $sys->lookup($word, 'de', 'en');
+ $fac = new Facade($c);
 
-       if (count($iter) == 0) {
-           
-            echo "$word has no definitions\n";
-                 
-            continue;
-       }
-       
-       echo "$word results:\n";
-       
-       foreach ($iter as $lookup_result)  {
-           
-          $word = $lookup_result->word_defined();
-             
-          if (!$db->word_exists($word)) {
-                
-              $db->save_lookup($lookup_result);
+ $fac->db_insert();
 
-              echo "$word saved to database.\n";
-          }
-
-          $sent_iter = $sentFetcher->fetch($word, $c->sentence_count());
-          
-          if ($sent_iter == false) { 
-              
-               echo "No sample sentences available for '$word'\n";
-               continue;
-          }
-
-          $db->save_samples($word, $sent_iter);  
-        }       
-    }
-
-} catch (PDOException $e) {
-
-      echo "Exception: message = " . $e->getMessage() . "\nError Code = " . $e->getCode() . "\n";
-}
-catch (Exception $e) {
-
-      echo "Exception: message = {$e->getMessage()}.\nError Code = {$e->getCode()}.\nException code = {$e->getCode()}.\n";
-}
+ $fac->create_html();
---
+++ src/Facade.php
@@ -0,0 +1,90 @@
+<?php
+declare(strict_types=1);
+use \SplFileObject as File;
+
+class Facade {
+   
+   private FileReader $file;
+
+   private SystranTranslator $sys;
+
+   private Database $db;
+
+   private LeipzigSentenceFetcher $sentFetcher;
+
+   private Config $c;
+
+   function __construct(string $fname, Config $c)
+   {
+      $this->file = new FileReader($c->lookup_file());
+      
+      $this->sys = new SystranTranslator($c);
+     
+      $this->db = new Database($c); 
+  
+      $this->sentFetcher = new LeipzigSentenceFetcher($c);
+
+      $this->c = $c; 
+   }
+  
+   function db_insert() int
+   {
+      try {
+       
+        foreach ($file as $word) {
+           
+           $iter = $this->sys->lookup($word, 'de', 'en');
+    
+           if (count($iter) == 0) {
+               
+                echo "$word has no definitions\n";
+                     
+                continue;
+           }
+           
+           echo "$word results:\n";
+           
+           foreach ($iter as $lookup_result)  {
+               
+              $word = $lookup_result->word_defined();
+                 
+              if (!$this->db->word_exists($word)) {
+                    
+                  $this->db->save_lookup($lookup_result);
+    
+                  echo "$word saved to database.\n";
+              }
+             
+             $this->insert_samples($word);
+           }
+        } 
+
+      } catch (PDOException $e) {
+      
+            echo "Exception: message = " . $e->getMessage() . "\nError Code = " . $e->getCode() . "\n";
+
+      } catch (Exception $e) {
+      
+            echo "Exception: message = {$e->getMessage()}.\nError Code = {$e->getCode()}.\nException code = {$e->getCode()}.\n";
+      }
+   } 
+ 
+   private function insert_samples(string $word)
+   { 
+      $sent_iter = $sentFetcher->fetch($word, $c->sentence_count());
+      
+      if ($sent_iter == false) { 
+          
+           echo "No sample sentences available for '$word'\n";
+           continue;
+      }
+   
+      $db->save_samples($word, $sent_iter);  
+   }
+
+ 
+    function create_html() : void
+    {
+    
+    }
+ }
