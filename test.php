2972be26036874625fb68f65aeaf5ec2dbf84fbd
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-02-14 09:18:06 -0500

    $1

--- test.php
+++ test.php
@@ -13,14 +13,11 @@ include 'vendor/autoload.php';
      die($c->lookup_file() . " not found.\n");
  }
  
- $cred = $c->get_db_credentials();
-     
- $pdo = new \PDO($cred["dsn"], $cred["user"], $cred["password"]); 
- 
- //$x = new Vocab\DBVerb($pdo, 1);
- 
- $y = new Vocab\NewDBVerb($pdo, 1);
 
- //$fac->db_insert();
+ $fac = new Facade($c);
+
+ $fac->db_insert();
+ 
+ //$y = new Vocab\NewDBVerb($pdo, 1);
 
  //--$fac->create_html('output');
