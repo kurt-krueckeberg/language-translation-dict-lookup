96176542b9fd00487895f3d30308262e38715dd2
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-02-15 16:12:55 -0500

    $1

--- src/AbstractDefinitionsIterator.php
+++ src/AbstractDefinitionsIterator.php
@@ -2,19 +2,41 @@
 declare(strict_types=1);
 namespace Vocab;
 
-abstract class AbstractDefinitionsIterator extends \ArrayIterator {
+abstract class AbstractDefinitionsIterator implements \Iterator {
 
-   function __construct(array $targets)
-   { 
-      parent::__construct($targets);
-   }
+    protected \ArrayIterator $aiter;
 
-   abstract protected function get_current(array $target) : DefinitionInterface | false;
+    abstract protected function get_current(array $target) : DefinitionInterface | false;
 
-   function current() : DefinitionInterface | false
-   {
+    public function __construct(array $arr)
+    {
+        $this->iter = new \ArrayIterator($arr);
+    }
+
+    function current() : DefinitionInterface | false
+    {
       $ele =  parent::current();
 
       return ($ele === false) ? $ele : $this->get_current($ele);
-   }
+    }
+
+    function next() : void
+    {
+       $this->iter->next();
+    }
+
+    function valid() : bool
+    {
+       return $this->iter->valid();
+    }
+
+    function key() : int | null
+    {
+       return $this->iter->key();
+    }
+
+    function rewind() : void
+    {
+       $this->iter->rewind();
+    }
 }
--- src/DBWordBase.php
+++ src/DBWordBase.php
@@ -4,12 +4,21 @@ namespace Vocab;
 
 // TODO: Get rid of AbstractDefinitionsIterator and put its iteation-related code in thie class.
 
-abstract class DBWordBase extends AbstractDefinitionsIterator {  
+abstract class DBWordBase implements \Iterator {  
 
    static private $stmts = [];
     
    protected \PDO $pdo;
-
+   
+   protected \ArrayIterator $aiter;
+   
+   function __construct(\PDO $pdo)
+   {
+      $this->pdo = $pdo;
+      
+      $this->iter = new \ArrayIterator($arr);
+   }
+   
    protected function get_stmt(\PDO $pdo, string $str) : \PDOStatement
    {     
       if (!isset(self::$stmts[$str])) {
@@ -27,13 +36,35 @@ abstract class DBWordBase extends AbstractDefinitionsIterator {
    abstract protected function bind(\PDOStatement $pdo, string $str) : void; 
 
    abstract protected function get_sql(string $str) : string;
- 
-   abstract protected function get_current(array $target) : DefinitionInterface | false;
+  
 
-   abstract function definitions() : AbstractDefinitionsIterator;
-   
-   function __construct(\PDO $pdo)
-   {
-      $this->pdo = $pdo;
-   }
+    abstract protected function get_current(array $target) : DefinitionInterface | false;
+
+    function current() : DefinitionInterface | false
+    {
+      $ele =  parent::current();
+
+      return ($ele === false) ? $ele : $this->get_current($ele);
+    }
+
+    function next() : void
+    {
+       $this->iter->next();
+    }
+
+    function valid() : bool
+    {
+       return $this->iter->valid();
+    }
+
+    function key() : int | null
+    {
+       return $this->iter->key();
+    }
+
+    function rewind() : void
+    {
+       $this->iter->rewind();
+    }
+ 
 }
