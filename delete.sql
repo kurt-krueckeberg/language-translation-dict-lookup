f262ad920245aa76ee2c13a157a7b5452a660e5c
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-02-19 13:31:30 -0500

    $1

--- delete.sql
+++
@@ -1,9 +0,0 @@
-use vocab;
-
-delete from exprs where 1;
-delete from defns where 1;
-delete from defns where 1;
-delete from nouns_data where 1;
-delete from conjugations where 1;
-delete from shared_conjugations where 1;
-delete from words where 1;
