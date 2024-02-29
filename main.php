99badb21f48d2ce070b50be0a756d622948accb9
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-02-29 11:28:36 -0500

    $1

--- main.php
+++ main.php
@@ -15,6 +15,14 @@ if (!file_exists($c->lookup_file())) {
 
 $fac = new Facade($c->lookup_file(), $c);
 
-$words = $fac->db_insert();
+//$words = $fac->db_insert();
+
+$file = new FileReader("/home/kurt/vocab-german/vocab.txt", "r");
+
+$words = [];
+
+foreach($file as $word) {
+    $words[] = $word;
+}
  
 $fac->create_html($words, 'output');
--- test.php
+++ test.php
@@ -1,21 +1,8 @@
 <?php
 declare(strict_types=1);
-use \SplFileObject as File;
-use Vocab\{AzureTranslator, SystranTranslator, Config};
+
+use Vocab\{CreateDBWordResultIterator, Pos};
 
 include 'vendor/autoload.php';
 
-try {
-    
-    $c = new Config();
-    
-    $azure = new AzureTranslator($c);
-
-    $english = $azure->translate("Guten Tag.", 'en', 'de');
-
-    echo $english . "\n";
-
-} catch (Exception $e) {
-
-      echo "Exception: message = " . $e->getMessage() . "\nError Code = " . $e->getCode() . "\n";
-  }
+$x = new CreateDBWordResultIterator(Pos::Verb, 1);
