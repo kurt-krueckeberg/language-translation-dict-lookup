0669bcbd013d631548c3fb18808c8148bdc55997
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-03-10 09:37:10 -0400

    $1

--- db_insert.php
+++ db_insert.php
@@ -17,49 +17,46 @@ try {
         die($c->lookup_file() . " not found.\n");
     }
 
-    $file = new FileReader($c->lookup_file());
-    
     $sys = new SystranTranslator($c);
    
     $db = new Database($c); 
 
     $sentFetcher = new LeipzigSentenceFetcher($c);
     
-    foreach ($file as $word) {
-       
-       $iter = $sys->lookup($word, 'de', 'en');
+    $word = "verursachen";
 
-       if (count($iter) == 0) {
+    $iter = $sys->lookup($word, 'de', 'en');
+
+    if (count($iter) == 0) {
+        
+         echo "$word has no definitions\n";
+              
+         continue;
+    }
+    
+    echo "$word results:\n";
+    
+    foreach ($iter as $lookup_result)  {
+        
+       $word = $lookup_result->word_defined();
+          
+       if (!$db->word_exists($word)) {
+             
+           $db->save_lookup($lookup_result);
+
+           echo "$word saved to database.\n";
+       }
+
+       $sent_iter = $sentFetcher->fetch($word, $c->sentence_count());
+       
+       if ($sent_iter == false) { 
            
-            echo "$word has no definitions\n";
-                 
+            echo "No sample sentences available for '$word'\n";
             continue;
        }
-       
-       echo "$word results:\n";
-       
-       foreach ($iter as $lookup_result)  {
-           
-          $word = $lookup_result->word_defined();
-             
-          if (!$db->word_exists($word)) {
-                
-              $db->save_lookup($lookup_result);
 
-              echo "$word saved to database.\n";
-          }
-
-          $sent_iter = $sentFetcher->fetch($word, $c->sentence_count());
-          
-          if ($sent_iter == false) { 
-              
-               echo "No sample sentences available for '$word'\n";
-               continue;
-          }
-
-          $db->save_samples($word, $sent_iter);  
-        }       
-    }
+       $db->save_samples($word, $sent_iter);  
+     }       
 
 } catch (PDOException $e) {
 
