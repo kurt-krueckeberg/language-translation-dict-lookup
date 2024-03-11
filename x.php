2c85e6bc5b5ff4953a76863227474b1c95e435be
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-03-10 20:34:24 -0400

    $1

--- x.php
+++ x.php
@@ -2,29 +2,11 @@
 declare(strict_types=1);
 use \SplFileObject as File;
 
-use Vocab\{FileReader, Config, Vocab};
+use Vocab\{FileReader, Config, AzureTranslate};
 
 include 'vendor/autoload.php';
-  
+
 $c = new Config();
-         
-if (!file_exists($c->lookup_file())) {
-     
-    die($c->lookup_file() . " not found.\n");
-}
 
-try {
+$az = new AzureTranslate($c);
 
-  $fac = new Vocab($c);
-
-  $words = $c->fetch_words();
-    
-  $words_inserted = $fac->db_insert($words);
-
-} catch (\Exception $e) {
-
-  echo $e->getMessage();
-
-  echo "-----\ngetting Trace as String: \n";
-  echo $e->getTraceAsString();
-}
