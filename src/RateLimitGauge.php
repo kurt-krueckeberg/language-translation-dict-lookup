4a59eacad814d7af7f60047ecc4c8989f82b0b9e
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2024-03-24 10:56:02 -0400

    $1

--- src/RateLimitGauge.php
+++ src/RateLimitGauge.php
@@ -10,6 +10,8 @@ class RateLimitGauge  {
 
   private static $hour_seconds = 3600;
 
+  private $time_start;
+
   function __construct(int $limit)
   {
      $this->char_cnt = 0;
@@ -17,34 +19,43 @@ class RateLimitGauge  {
      $this->hourly_limit = $limit;
 
      $this->time_start = \time();
+
+     $this->wait = $wait;
   }
 
   function reset() : void
   {
      $this->char_cnt = 0; 
+     $this->time_start = \time();
   }
 
-  function willExceed(string $input) : bool
+  private function willExceed(int $str_length) : bool
   {
-     return (\strlen($input) + $this->char_cnt >= $this->hourly_limit) ? true: false;
+     return ($str_length + $this->char_cnt >= $this->hourly_limit) ? true: false;
   }
 
-  function Exceeded() : bool
-  {
-     return ($this->char_cnt >= $this->hourly_limit) ? true: false;
-  }
-
-  function __invoke(string $input)
+  function __invoke(string $input) : bool
   {
       $this->char_cnt += \strlen($input);
 
-      $this->current_time = \time();  
-
-      if ($this->current_time > self::$hour_seconds) {
+      if (\time() - $this->time_start > self::$hour_seconds) {
 
           $this->reset();
       }
+
+      $str_len = \strlen($input);
  
+      if ($this->willExceed($str_len)) {
+      
+          $rc = false;
+
+      } else {
+
+          $this->char_cnt += $str_len;
+          $rc = true;
+      }  
+
+      return $rc;
   }
 }
 
